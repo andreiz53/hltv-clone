8c416b1c501b9f6eea86c789671b59e73ee76e3a
andreiz53 <andrei.zamfira53@gmail.com>
2023-06-10 13:17:24 +0300

    fix: previous commit is bad

--- src/components/results/SingleResultPreview.vue
+++ src/components/results/SingleResultPreview.vue
@@ -3,7 +3,7 @@ defineProps(['result'])
 </script>
 
 <template>
-  <div class="result-container">
+  <router-link :to="`/results/${result.matchId}`" class="result-container">
     <div class="result-team-container">
       <p class="result-team">{{ result.teams[0].name }}</p>
       <p class="result-score">{{ result.teams[0].result }}</p>
@@ -12,7 +12,7 @@ defineProps(['result'])
       <p class="result-team">{{ result.teams[1].name }}</p>
       <p class="result-score">{{ result.teams[1].result }}</p>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <style scoped>
@@ -20,7 +20,7 @@ defineProps(['result'])
   @apply flex justify-between items-center;
 }
 .result-container {
-  @apply grid gap-1 bg-primary-200 rounded py-1 px-2;
+  @apply grid gap-1 bg-primary-200 rounded py-1 px-2 hover:bg-primary-300 transition;
 }
 .result-team {
   @apply text-base font-semibold;
